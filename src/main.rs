e26a05b57793d132e50f606fdf395e242dd5a449
Pavel2202 <paveltimenov3@gmail.com>
2023-07-10 10:23:45 +0300

    Create simple get request

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use actix_web::{get, web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+
+struct AppState {
+    movies: Mutex<Vec<Movie>>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Movie {
+    id: i32,
+    title: String
+}
+
+#[get("/")]
+async fn index() -> String {
+    "It is working".to_string()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app_data = web::Data::new(AppState {
+        movies: Mutex::new(vec![])
+    });
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(app_data.clone())
+        .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
