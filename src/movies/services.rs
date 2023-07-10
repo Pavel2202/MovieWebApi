bd9ed7d9f9f8203c3636465c548c3b5fb6f65812
Pavel2202 <paveltimenov3@gmail.com>
2023-07-10 13:11:49 +0300

    Create put and delete endpoints

--- src/movies/services.rs
+++ src/movies/services.rs
@@ -26,7 +26,33 @@ async fn create_movie(data: web::Data<AppState>, param_obj: web::Json<CreateMovi
     HttpResponse::Ok().json(movies.to_vec())
 }
 
+#[put("/movies/{id}")]
+async fn update_movie(data: web::Data<AppState>, path: web::Path<i32>, param_obj: web::Json<UpdateMovie>) -> impl Responder {
+    let id: i32 = path.into_inner();
+    let mut movies = data.movies.lock().unwrap();
+
+    for i in 0..movies.len() {
+        if movies[i].id == id {
+            movies[i].title = param_obj.title.clone();
+            break;
+        }
+    }
+
+    HttpResponse::Ok().json(movies.to_vec())
+}
+
+#[delete("/movies/{id}")]
+async fn delete_movie(data: web::Data<AppState>, path: web::Path<i32>) -> impl Responder {
+    let id: i32 = path.into_inner();
+    let mut movies = data.movies.lock().unwrap();
+    *movies = movies.to_vec().into_iter().filter(|x| x.id != id).collect();
+
+    HttpResponse::Ok().json(movies.to_vec())
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(get_movies)
-    .service(create_movie);
+    .service(create_movie)
+    .service(update_movie)
+    .service(delete_movie);
 }
